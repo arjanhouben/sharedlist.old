a80a7b370ef21590d2902f271a7a1a4eb5a75cab
arjan <github.com@arjanhouben.nl>
2015-02-08 01:12:46 +0100

    reordered php handling for more efficiency, introduced sessions so timestamps are irrelevant for when the same session is used.

--- sync.php
+++ sync.php
@@ -1,44 +1,22 @@
 <?php
 
 $unique = 0;
+$error = array();
+$result = array();
+
+if ( array_key_exists( "session", $_REQUEST ) )
+{
+	$session = $_REQUEST[ "session" ];
+}
+else
+{
+	$session = md5( $_SERVER[ "HTTP_USER_AGENT" ] . $_SERVER[ "REMOTE_ADDR" ] . uniqid() );
+}
 
 if ( !file_exists( "items" ) )
 {
 	mkdir( "items" );
 }
-
-$client = $_SERVER[ "HTTP_USER_AGENT" ] . $_SERVER[ "REMOTE_ADDR" ];
-
-if ( array_key_exists( "items", $_REQUEST ) )
-{
-	foreach( $_REQUEST[ "items" ] as $name => $item )
-	{
-		if ( is_array( $item ) && key_exists( "modified", $item ) )
-		{
-			$path = "items/" . $name;
-			$date = $item[ "modified" ];
-			$item[ "owner" ] = $client;
-			unset( $item[ "modified" ] );
-			$newcontent = json_encode( $item );
-			if ( file_exists( $path ) )
-			{
-				if ( filemtime( $path ) != $date )
-				{
-					continue;
-				}
-			}
-			
-			file_put_contents( $path, $newcontent );
-		}
-	}
-}
-
-if ( array_key_exists( "id", $_REQUEST ) )
-{
-	$unique = $_REQUEST[ "id" ];
-}
-
-$result = Array();
 
 if ( $dh = opendir( "items" ) )
 {
@@ -56,6 +34,55 @@ if ( $dh = opendir( "items" ) )
 	closedir( $dh );
 }
 
-echo json_encode( [ "items" => $result, "id" => $unique ] );
+if ( array_key_exists( "items", $_REQUEST ) )
+{
+	foreach( $_REQUEST[ "items" ] as $name => &$item )
+	{
+		if ( is_array( $item ) && key_exists( "modified", $item ) && key_exists( "state", $item ) )
+		{
+			$path = "items/" . $name;
+			$current = &$result[ $name ];
+			
+			if ( file_exists( $path ) )
+			{
+				if ( $current[ "state" ] == $item[ "state" ] )
+				{
+					$error[ $name ] = "redundant change, state remains at " . $current[ "state" ];
+					continue;
+				}
+				
+				if ( key_exists( "session", $current ) && $current[ "session" ] != $session )
+				{
+					if ( $current[ "modified" ] != $item[ "modified" ] )
+					{
+						$error[ $name ] = "could not be saved, state remains at " . $result[ "modified" ];
+						continue;
+					}
+				}
+			}
+
+			unset( $item[ "modified" ] );
+			$item[ "session" ] = $session;
+			
+			file_put_contents( $path, json_encode( $item ) );
+			
+			$current = $item;
+			$current[ "modified" ] = filemtime( $path );
+		}
+	}
+}
+
+foreach( $result as $name => &$item )
+{
+	unset( $item[ "session" ] );
+}
+
+echo json_encode(
+	[
+		"items" => $result,
+		"errors" => $error,
+		"session" => $session
+	]
+);
 
 ?>

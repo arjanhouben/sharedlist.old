05aeaf472203a8036ba555510b4fe05edba2b9f4
Arjan Houben <git@arjanhouben.nl>
2020-07-24 00:02:36 +0200

    rewrite to use sqlite db

--- sync.php
+++ sync.php
@@ -1,8 +1,11 @@
 <?php
 
 $error = array();
-$server_data = array();
+$result = array();
+$db = new SQLite3( "items/sqlite.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE );
 $request = array();
+
+$db->exec( "CREATE TABLE IF NOT EXISTS items ( name varchar(255), modified float, usecount int, state int )" );
 
 if ( $_REQUEST )
 {
@@ -11,11 +14,6 @@ if ( $_REQUEST )
 		$json = $_REQUEST[ "json" ];
 		$request = json_decode( $json, true );
 	}
-}
-
-if ( !file_exists( "items" ) )
-{
-	mkdir( "items" );
 }
 
 $lastModified = -1;
@@ -27,84 +25,90 @@ if ( $request )
 		$lastModified = floatval( $request[ "lastModified" ] );
 	}
 }
-		
-if ( $dh = opendir( "items" ) )
+
+$sql_result = $db->query( "SELECT * FROM items WHERE modified > " . $lastModified );
+
+while( $row = $sql_result->fetchArray( SQLITE3_ASSOC ) )
 {
-	while ( ( $file = readdir( $dh ) ) !== false )
-	{
-		$path = "items/" . $file;
-		if ( is_file( $path ) )
-		{
-			if ( filemtime( $path ) > $lastModified )
-			{
-				$server_data[ $file ] = json_decode( file_get_contents( $path ), true );
-				if ( $server_data[ $file ] )
-				{
-					if ( !array_key_exists( "modified", $server_data[ $file ] ) )
-					{
-						$server_data[ $file ][ "modified" ] = 0;
-					}
-				}
-			}
-		}
-	}
-	
-	closedir( $dh );
+	$name = $row[ "name" ];
+	unset( $row[ "name" ] );
+	$result[ "items" ][ $name ] = $row;
 }
+
+// check if user sent items
 
 if ( $request )
 {
 	if ( array_key_exists( "items", $request ) )
 	{
 		$currentModificationTime = microtime( true );
-		
+
 		foreach( $request[ "items" ] as $name => &$item )
 		{
-			if ( is_array( $item ) )
+			unset( $result[ $name ] );
+		
+			// get server version of same item
+			$WHERE_CLAUSE = " WHERE name=\"" . $name . "\"";
+			$sql_result = $db->query( "SELECT modified,state,usecount FROM items" . $WHERE_CLAUSE );
+			$current_server_entry = $sql_result->fetchArray( SQLITE3_ASSOC );
+
+			if ( $current_server_entry )
 			{
-				$current_server_entry = &$server_data[ $name ];
-				
-				if ( $current_server_entry )
+				if ( $current_server_entry[ "state" ] == $item[ "state" ] )
 				{
-					if ( $current_server_entry[ "state" ] == $item[ "state" ] )
+					$error[ $name ] = "redundant change, state remains at " . $current_server_entry[ "state" ];
+					$item = $current_server_entry;
+					$result[ "items" ][ $name ] = $item;
+					continue;
+				}
+			
+				if ( array_key_exists( "modified", $current_server_entry ) )
+				{
+					if ( floatval( $current_server_entry[ "modified" ] ) > 0 )
 					{
-						$error[ $name ] = "redundant change, state remains at " . $current_server_entry[ "state" ];
-						continue;
-					}
-				
-					if ( array_key_exists( "modified", $current_server_entry ) )
-					{
-						if ( $current_server_entry[ "modified" ] != $item[ "modified" ] )
+						// bccomp returns 0 if values match
+						if ( bccomp( $current_server_entry[ "modified" ], $item[ "modified" ], 4 ) )
 						{
-							$error[ $name ] = "could not be saved, modified " . $current_server_entry[ "modified" ] . " differs from " . $item[ "modified" ];
+							$error[ $name ] = "could not be saved, modified " .
+								$current_server_entry[ "modified" ] .
+								" differs from " . $item[ "modified" ];
+							$error[ "modified" ] = $current_server_entry[ "modified" ];
 							continue;
 						}
 					}
 				}
-				
-				$path = "items/" . $name;
-				
-				$current_server_entry[ "modified" ] = $currentModificationTime;
+			}
+			else
+			{
+				$current_server_entry[ "usecount" ] = 0;
+			}
 			
-				$current_server_entry[ "state" ] = $item[ "state" ];
-				
-				if ( !array_key_exists( "usecount", $current_server_entry ) )
-				{
-					$current_server_entry[ "usecount" ] = 0;
-				}
-				$current_server_entry[ "usecount" ] = intval( $current_server_entry[ "usecount" ] ) + 1;
-				
-				file_put_contents( $path, json_encode( $current_server_entry ) );
+			$item[ "modified" ] = $currentModificationTime;
+			$item[ "usecount" ] = intval( $current_server_entry[ "usecount" ] ) + 1;
+			
+			if ( $db->exec(
+					"UPDATE items SET modified = \"" . $item[ "modified" ] .
+					"\", state = \"" . $item[ "state" ] .
+					"\", usecount = \"" . $item[ "usecount" ] . "\"" . $WHERE_CLAUSE
+					)
+				)
+			{
+				$result[ "items" ][ $name ] = $item;
+			}
+			else
+			{
+				$result[ "items" ][ $name ] = $current_server_entry;
+				$error[ $name ] = "could not be saved, DB did not accept change: "
+					. $db->lastErrorCode()
+					. " -> "
+					. $db->lastErrorMsg();
 			}
 		}
 	}
 }
-
-echo json_encode(
-	[
-		"items" => $server_data,
-		"errors" => $error
-	]
-);
+	
+$result[ "errors" ] = $error;
+echo json_encode( $result );
 
 ?>
+
